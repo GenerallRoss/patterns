150ebafb220e2da60811b534f3642f6270069cd6
Dmitry <dmitry.ananev2000@gmail.com>
2022-08-23 13:10:55 +0700

    Исправлен комментарий

--- lib/MyBuilder/my_builder.dart
+++ lib/MyBuilder/my_builder.dart
@@ -5,11 +5,11 @@ import 'classes.dart';
 void runMyBuilder() {
   Baker baker = Baker();
   // создаем билдер для ржаного хлеба
-  BreadBuilder builder = new RyeBreadBuilder();
+  BreadBuilder builder = RyeBreadBuilder();
   // выпекаем
   Bread ryeBread = baker.bake(builder);
   debugPrint(ryeBread.printResult());
-  // оздаем билдер для пшеничного хлеба
+  // создаем билдер для пшеничного хлеба
   builder = WheatBreadBuilder();
   Bread wheatBread = baker.bake(builder);
   debugPrint(wheatBread.printResult());
